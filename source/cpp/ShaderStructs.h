0c90795d316b64bff25619f29ade8cf9700b850c
throwthisaway <throwthisaway@freemail.hu>
2018-05-13 13:09:44 +0200

    more renaming

---
+++ source/cpp/ShaderStructs.h
@@ -0,0 +1,34 @@
+#ifndef __SHADERSTRUCTS___
+#define __SHADERSTRUCTS___
+struct Object {
+	float4x4 mvp;
+	float4x4 m;
+};
+struct Material {
+	float3 diffuse;
+	float pad1;
+	float specular, power;
+};
+struct PointLight {
+	float3 diffuse;
+	float pad1;
+	float3 ambient;
+	float pad2;
+	float3 specular;
+	float pad3;
+	float3 pos;
+	float pad4;
+	float3 att;
+	float pad5;
+	float range;
+	float3 pad6;
+};
+#define MAX_LIGHTS 2
+struct Scene {
+	float4x4 ip;
+	PointLight light[MAX_LIGHTS];
+	float3 eyePos;
+	float pad0;
+	float n, f;
+};
+#endif
