de220244a4221d3617e9a5e92054a7dc68355036
Andras Kovacs <andras.kovacs@prezi.com>
2019-08-24 18:07:35 +0200

    fix normal mapping

--- FireAndForget.v2/Content/shaders/ModoDNPS.hlsl
+++ FireAndForget.v2/Content/shaders/ModoDNPS.hlsl
@@ -21,12 +21,12 @@ MRTOut main(PS_UVNT input) {
 	float3 nTex = tNormal.Sample(smp, input.uv).rgb * 2.f - 1.f;
 	float3 n = normalize(input.n);
 	float3 t = input.t;
-	t = normalize(t - dot(n, t)*n);
-	float3 b = cross(t, n);
+	t = normalize(t - dot(t, n)*n);
+	float3 b = cross(n, t);
 	float3x3 tbn = float3x3(t, b, n);
-	n = mul(tbn, nTex);
+	n = mul(nTex, tbn);
 	output.normal = Encode(n);
 	output.material = float4(mat.metallic_roughness, 0.f, 1.f);
-	output.debug = float4(n, 1.f);
+	output.debug = float4(nTex, 1.f);
 	return output;
 }
